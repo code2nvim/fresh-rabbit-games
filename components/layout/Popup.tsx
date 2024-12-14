f380004523396d9092ba956e304bbb1442f45463
code2nvim <code2nvim@gmail.com>
2024-12-14 19:18:21 +0800

    Whack: init and add anchor to Popup

--- components/layout/Popup.tsx
+++ components/layout/Popup.tsx
@@ -1,14 +1,19 @@
+import { JSX } from "preact";
+
+function Anchor(props: JSX.HTMLAttributes<HTMLAnchorElement>) {
+  return (
+    <a
+      {...props}
+      class="h-12 w-52 rounded-md border-2 border-black bg-green-600"
+    />
+  );
+}
+
 export function Popup() {
   return (
-    <ul class="rounded-xl border-2 border-white bg-green-900 p-8 text-black opacity-100">
-      <li>
-        <a
-          href="/"
-          class="rounded-md border-2 border-black bg-green-600 px-16 py-2"
-        >
-          Home
-        </a>
-      </li>
+    <ul class="flex flex-col gap-4 rounded-xl border-2 border-white bg-green-900 p-8 text-black opacity-100">
+      <Anchor href="/">Home</Anchor>
+      <Anchor href="/whack">Whack</Anchor>
     </ul>
   );
 }
---
+++ routes/whack.tsx
@@ -0,0 +1,7 @@
+export default function WhackGame() {
+  return (
+    <main class="flex flex-grow items-center justify-center">
+      <h1>WhackGame</h1>
+    </main>
+  );
+}
