990b6f91a096599f2cba06ff372258d5f641f4ed
code2nvim <code2nvim@gmail.com>
2024-12-15 20:05:30 +0800

    server: let port, cert, key be set by environment variables

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 
+const CERT = Deno.env.get("CERT");
+const KEY = Deno.env.get("KEY");
+
 export default defineConfig({
   plugins: [tailwind()],
-  port: 3000,
-  cert: Deno.readTextFileSync("./.env.local/public_certificate.pem"),
-  key: Deno.readTextFileSync("./.env.local/private_key.pem"),
+  server: {
+    cert: CERT && Deno.readTextFileSync(CERT),
+    key: KEY && Deno.readTextFileSync(KEY),
+  },
 });
