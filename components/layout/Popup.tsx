422b9cbced05b66fde1accb96bcd252ced71e58b
code2nvim <code2nvim@gmail.com>
2024-12-17 23:00:42 +0800

    Popup: center text

--- components/layout/Popup.tsx
+++ components/layout/Popup.tsx
@@ -4,14 +4,14 @@ function Anchor(props: JSX.HTMLAttributes<HTMLAnchorElement>) {
   return (
     <a
       {...props}
-      class="h-12 w-52 rounded-md border-2 border-black bg-green-600"
+      class="h-10 w-52 flex justify-center items-center rounded-md border border-black bg-green-600"
     />
   );
 }
 
 export function Popup() {
   return (
-    <ul class="flex flex-col gap-4 rounded-xl border-2 border-white bg-green-900 p-8 text-black">
+    <ul class="flex flex-col gap-4 rounded-xl border-2 border-white bg-green-900 p-6 text-black">
       <Anchor href="/">Home</Anchor>
       <Anchor href="/whack">Whack</Anchor>
     </ul>
