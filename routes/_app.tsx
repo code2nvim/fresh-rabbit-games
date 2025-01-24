080635b1c9d4748ba7047ac70d47f5bff57ea03a
code2nvim <code2nvim@gmail.com>
2025-01-24 19:55:36 +0800

    move metadata from Layout to App

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,8 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>fresh-rabbit-games</title>
+        <title>Fresh Rabbit Games</title>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -1,5 +1,4 @@
 import type { PageProps } from "$fresh/server.ts";
-import { Head } from "$fresh/runtime.ts";
 import { Footer } from "../components/layout/Footer.tsx";
 import { Header } from "../components/layout/Header.tsx";
 import { PopupScreen } from "../islands/layout/Popup.tsx";
@@ -7,10 +6,6 @@ import { PopupScreen } from "../islands/layout/Popup.tsx";
 export default function Layout({ Component }: PageProps) {
   return (
     <>
-      <Head>
-        <title>Fresh Rabbit Games</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div class="flex bg-black">
         <div class="flex h-screen w-full flex-col justify-between bg-green-300">
           <Header />
